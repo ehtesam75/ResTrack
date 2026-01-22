ffd59d4606019bc474eb71b78805a56b9fa8434f
Ehtesam Zunnuryn <127880759+ehtesam75@users.noreply.github.com>
2026-01-22 23:43:33 +0600

    Update static file serving in development
    Removed media files serving from urlpatterns in development.

--- ResTrack/urls.py
+++ ResTrack/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', include('marks.urls')),
 ]
 
-# Serve static and media files in development
+# Serve static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
